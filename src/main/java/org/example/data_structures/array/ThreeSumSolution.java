f81ce928848177d4a3e8c2665517a138fef5a6c6
baba_yaga <ga041386@gmail.com>
2024-11-28 12:55:50 +0530

    Redo three sum problem

--- src/main/java/org/example/data_structures/array/ThreeSumSolution.java
+++ src/main/java/org/example/data_structures/array/ThreeSumSolution.java
@@ -7,31 +7,46 @@ import java.util.List;
 
 public class ThreeSumSolution {
 
-  public List<List<Integer>> findTriplets(int[] arr) {
-    List<List<Integer>> result = new ArrayList<>();
-    new QuickSort().sort(arr);
+    public List<List<Integer>> findTriplets(int[] arr) {
+        // Sort the array
+        // Fix i at 0, j at i + 1, k at arr.length - 1
+        // Check if arr[i] + arr[j] + arr[k] is equal to 0. If true, it is the target triplet. Add it to result.
+        // If false, check if sum is greater than 0. Then we need a smaller sum in the next iteration. So, we move k towards left.
+        // If false, check if sum is smaller than 0. Then we need a greater sum in the next iteration. So, we move j towards right.
+        // Then we move i and loop remaining values of arr[i] to check the same.
+        // Improvement in processing time: Skip elements when arr[i] == arr[i-1]. They will have the same sum as previous iteration.
 
-    for (int i = 0; i < arr.length; i++) {
-      if (i > 0 && arr[i] == arr[i - 1]) {
-        continue;
-      }
-      int j = i + 1;
-      int k = arr.length - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        new QuickSort().sort(arr);
+        int length = arr.length;
+        int i = 0;
 
-      while (j < k) {
-        int sum = arr[i] + arr[j] + arr[k];
-        if (sum == 0) {
-          result.add(List.of(arr[i], arr[j], arr[k]));
-          do {
-            j++;
-          } while (arr[j] == arr[j - 1]);
-        } else if (sum < 0) {
-          j++;
-        } else {
-          k--;
+        while (i < length) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                i++;
+                continue;
+            }
+
+            int j = i + 1;
+            int k = length - 1;
+
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+
+                if (sum == 0) {
+                    result.add(List.of(arr[i], arr[j], arr[k]));
+                    do {
+                        j++;
+                    } while (j < k && arr[j] == arr[j - 1]);
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+            i++;
         }
-      }
+
+        return result;
     }
-    return result;
-  }
 }
